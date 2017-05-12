2f848ee6f0c3224580039d0a9626c238336479c5
HZWCMD <1747506378@qq.com>
2017-05-12 09:11:55 +0800

    开始界面交互2017-05-12
    添加加载画面

---
+++ Assets/Scripts/UI/UILoad.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UILoad : MonoBehaviour {
+    public string startscene;
+    public AsyncOperation async;
+    public int progress=0;
+    public Text loadtext;
+    public Image loadimg; 
+	// Use this for initialization
+	void Start () {
+        startScene(startscene);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        progress = (int)(async.progress * 100);
+        loadtext.text = "加载"+progress + "%";
+        loadimg.fillAmount = async.progress;
+	}
+    public void startScene(string sceneName)
+    {
+        StartCoroutine("LoadScene", sceneName);
+    }
+    IEnumerator LoadScene(string sceneName) {
+        async = SceneManager.LoadSceneAsync(sceneName);
+        async.allowSceneActivation = true;
+        yield return new WaitForEndOfFrame();
+        yield return async;
+    }
+
+}
---
+++ Assets/Scripts/UI/startmanager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startmanager : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void onstartgame(string startscene) {
+        SceneManager.LoadScene(startscene);
+    }
+    public void onsavegame(string savescene)
+    {
+        SceneManager.LoadScene(savescene);
+    }
+    public void onoptiongame(string optionscene)
+    {
+        SceneManager.LoadScene(optionscene);
+    }
+    public void onquitgame()
+    {
+        Application.Quit();
+    }
+}
